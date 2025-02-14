e6e33dedb6a8612f52f57d3008af1bf472754389
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2025-02-14 02:26:39 -0500

    initial commit

---
+++ handler.py
@@ -0,0 +1,12 @@
+import json
+
+def hello(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({
+            'message': 'Hello, World!'
+        })
+    } 
